932db21d517fb6337c9bde1f44dfe6f7a9b82c17
NuSnips <mail.nuwanthi@gmail.com>
2023-08-15 19:03:23 +0530

    Initial commit

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+    <div id="main-wrapper">
+        <header>Header</header>
+        <main>
+            <slot />
+        </main>
+        <footer>&copy; 2023</footer>
+    </div>
+</template>
---
+++ resources/js/Pages/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+    <h1>Content</h1>
+</template>
